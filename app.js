f9c08b0a78de60b1e342c4302074e4553483df12
Jyotiraditya <132455858+Jyotiraditya-lal@users.noreply.github.com>
2023-09-08 21:42:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const bodyParser = require('body-parser');
 const express=require('express')
-const admin=require('./Routes/admin')
-const shop=require('./Routes/shop')
+const loginRoute=require('./Routes/login')
+const MessageRouter=require('./Routes/MessageForm')
 
 const app=express()
 
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use('/admin',admin)
-app.use('/shop',shop)
+app.use(loginRoute)
+app.use(MessageRouter)
+
+app.use('/',(req,res,next)=>{
+    res.send('<h1>Welcome to Group Chat App</h1>')
+})
 
 app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page not found</h1>')
+   res.status(404).send('<h1>Page not found</h1>')
 })
 
 app.listen(3000)
